9bcfaa58c508ef167b5a52ad0cfd49d29b275777
ilya latushko <ilyalatt@gmail.com>
2019-01-23 03:06:12 +0300

    enhance the deploy script

--- deploy-sns-index.sh
+++ deploy-sns-index.sh
@@ -6,6 +6,5 @@ ep=root@sns-index.com
 ssh $ep "cd sns-index && cp -r bin bin-new"
 rsync -r build/. $ep:/root/sns-index/bin-new
 rm -rf build
-ssh $ep "pkill dotnet"
-ssh $ep "cd sns-index && rm -rf bin && mv bin-new bin"
+ssh $ep "(pkill dotnet || true) && cd sns-index && rm -rf bin && mv bin-new bin"
 ssh $ep "cd sns-index && ./run.sh"
