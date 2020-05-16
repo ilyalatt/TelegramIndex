a9ae9d76f5895942a96dae353cdb2f2fa2f6cf5d
Ilya Latushko <ilyalatt@gmail.com>
2020-05-16 19:25:25 +0300

    remove https

--- src/api/WebApp.fs
+++ src/api/WebApp.fs
@@ -2,19 +2,13 @@ module TelegramIndex.WebApp
 
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
-open Microsoft.AspNetCore.Server.Kestrel.Https
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
 open System
-open System.Net
 
 
-let configureApp isProd iface (app: IApplicationBuilder) =
+let configureApp iface (app: IApplicationBuilder) =
     do Api.configureApp app iface
-    if isProd then
-        do ignore <| app
-            .UseHsts()
-            .UseHttpsRedirection()
     do ignore <| app.UseDefaultFiles().UseStaticFiles()
 
 let configureServices (services: IServiceCollection) =
@@ -22,32 +16,16 @@ let configureServices (services: IServiceCollection) =
     do ignore <| services
 
 let run iface (args: string[]) =
-    let envKey = WebHostDefaults.EnvironmentKey
     let config =
         ConfigurationBuilder()
-            .AddInMemoryCollection([ Collections.Generic.KeyValuePair<string, string>(envKey, "Development") ])
             .AddCommandLine(args)
             .Build()
-    let envVal = config.[envKey]
-    let isProd = envVal = "Production"
     
     let webHostBuilder = WebHostBuilder()
     do ignore <| webHostBuilder
         .UseKestrel()
-        .ConfigureKestrel(fun o ->
-            if isProd then
-                do o.Listen(IPAddress.Any, 80)
-                do o.Listen(IPAddress.Any, 443, fun o ->
-                    do ignore <| o.UseHttps("/root/sns-index/https.pfx")
-                    ()
-                )
-                o.ConfigureHttpsDefaults(fun o ->
-                    do o.ClientCertificateMode <- ClientCertificateMode.RequireCertificate
-                )
-            ()
-         )
         .UseConfiguration(config)
-        .Configure(Action<IApplicationBuilder> (configureApp isProd iface))
+        .Configure(Action<IApplicationBuilder> (configureApp iface))
         .ConfigureServices(configureServices)
     webHostBuilder
         .Build()
